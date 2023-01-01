da0e366bf6435ee60b6aa4dfcfb0022b63b14b0b
sudoskys <coldlando@hotmail.com>
2023-01-02 01:27:58 +0800

    init

--- src/openai_kira/__init__.py
+++ src/openai_kira/__init__.py
@@ -8,3 +8,11 @@ from .resouce import Completion
 
 from .setting import RedisConfig
 from .setting import openaiApiKey, redisSetting, dbFile, proxyUrl, webServerUrlFilter, webServerStopSentence
+
+RedisConfig = RedisConfig
+openaiApiKey = openaiApiKey
+redisSetting = redisSetting
+dbFile = dbFile
+proxyUrl = proxyUrl
+webServerUrlFilter = webServerUrlFilter
+webServerStopSentence = webServerStopSentence
